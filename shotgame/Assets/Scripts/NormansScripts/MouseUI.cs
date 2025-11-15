56443ff77ea34bedbe0582456c5aba2f78a5468b
nmmn4947 <nmmn4947@gmail.com>
2025-11-15 11:08:22 -0800

    ui2

--- shotgame/Assets/Scripts/NormansScripts/MouseUI.cs
+++ shotgame/Assets/Scripts/NormansScripts/MouseUI.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MouseUI : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,9 @@ public class MouseUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f; // important for 2D
         
+        transform.position = Input.mousePosition;
     }
 }
